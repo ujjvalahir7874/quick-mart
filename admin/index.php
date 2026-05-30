9e0edc7a64ab84eb90908a15a60d21aa4e6db4b6
ujjvalahir7874 <ujjvalahir7874@gmail.com>
2026-05-30 15:31:53 +0530

    Fix admin root redirect

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,4 @@
 <?php
-// Get the directory path to ensure we redirect to the correct location
-$dir = dirname($_SERVER['PHP_SELF']);
-// If the path ends in index.php (like in index.php/dashboard.php), clean it up
-$clean_dir = preg_replace('/index\.php.*$/', '', $dir);
-header("Location: " . $clean_dir . "dashboard.php");
+header("Location: dashboard.php");
 exit;
 ?>
